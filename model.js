6d9dc854acc0409e61cf14640410d08ccfbf3e02
Waqar1982 <waquar82@gmail.com>
2025-03-24 14:40:57 +0100

    a

---
+++ model.js
@@ -0,0 +1,39 @@
+const model= {
+
+app:{
+    page: ['startPage', 'dashbord', 'RatingPage','profil','ownerPage','dogPage','topLists']
+
+
+},
+inputs: {
+    loggInn: {
+        userName: '',
+        password: '',
+    },
+   
+
+},
+
+
+
+
+data: {
+
+adminUser: {
+name: '',
+password: '',
+},
+
+
+
+
+
+},
+
+
+
+
+
+
+    
+};
