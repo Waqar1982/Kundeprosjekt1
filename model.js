de680b8fa44807bd034b1ad96942f6380d21e043
Waqar1982 <waquar82@gmail.com>
2025-03-25 10:02:02 +0100

    a

--- model.js
+++ model.js
@@ -1,7 +1,14 @@
-const model= {
+const model = {
 
 app:{
-    page: ['startPage', 'dashbord', 'RatingPage','profil','ownerPage','dogPage','topLists']
+    page: [
+        'startPage', 
+        'dashboard', 
+        'RatingPage',
+        'profil',
+        'ownerPage',
+        'dogPage',
+        'topLists']
 
 
 },
@@ -9,31 +16,60 @@ inputs: {
     loggInn: {
         userName: '',
         password: '',
+        infoOwner: '',
+    },
+    
+    searchResult: {
+        sort: null,
+        searchText: '',
+        layout: 'list',
     },
    
+    createUser: {
+        username: '',
+        password: '',
+        email: '',
+        firstName: '',
+        lastName: '',
+    },
 
+    myProfile: {
+        myInfo: '',
+        newDog: '',
+        addImgDog: '',
+        profilePicture: '',
+    },
+
+    rateDog: {
+        giveStars: null,
+        commentDog: '',
+    },
 },
-
-
-
 
 data: {
+    users: [{
+        userID: 5,
+        adminUser: false,
+        firstName: '',
+        lastName: '',
+        password: '',
+        email: '',
+        profilePicture: '',
+        dogList: [],
+        info: '',
+        dogRatings: [{id: 1, rating: 4.5}]
+    }],
 
-adminUser: {
-name: '',
-password: '',
+    dogs: [
+        { id: 1, rase: '', alder: '', størrelseVekt: '', favorittmat: '', rating: 4, pictures: [] },
+        { id: 2, rase: '', alder: '', størrelseVekt: '', favorittmat: '', rating: 3, pictures: [] },
+        { id: 3, rase: '', alder: '', størrelseVekt: '', favorittmat: '', rating: 3.5, pictures: [] },
+        { id: 4, rase: '', alder: '', størrelseVekt: '', favorittmat: '', rating: 5, pictures: [] },
+        { id: 5, rase: '', alder: '', størrelseVekt: '', favorittmat: '', rating: 4.5, pictures: [] },
+    ],
+
+    nyheter: [],
+
 },
 
-
-
-
-
-},
-
-
-
-
-
-
-    
-};
+}
