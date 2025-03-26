bf051cd46bd0bd0d709a92bc256dea82b115d35a
Waqar1982 <waquar82@gmail.com>
2025-03-26 13:37:25 +0100

    Update dashboardView.js

--- views/dashboardView.js
+++ views/dashboardView.js
@@ -1,4 +1,4 @@
-function dasboardView() {
+function dashboardView() {
     app.innerHTML = /*HTML*/ `
     <button onclick="topLists()">Flere Topplister</button>
     <input type="text" id="search" placeholder="Søk">
