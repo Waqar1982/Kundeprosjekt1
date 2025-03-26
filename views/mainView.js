daba4266cc6631a2f4ce386cf3db52b77a14f388
Waqar1982 <waquar82@gmail.com>
2025-03-26 13:01:18 +0100

    a

--- views/mainView.js
+++ views/mainView.js
@@ -4,6 +4,20 @@ mainView ()
 function mainView(){
     app.innerHTML=/*HTML*/`
     <button onclick= "ratingStarsView()">ratingStarsView</button>
+    <button onclick= "dasboardView()">dashboardView</button>
+    <button onclick= "dogPageView()">dogPageView</button>
+    <button onclick= "dogRatingView()">dogRatingView</button>
+    <button onclick= "ownerPagerView()">ownerPagerView</button>
+    <button onclick= "startPageView()">startPageView</button>
+    <button onclick= "topListsView()">topListsView</button>
+    <button onclick= "userProfileView()">userProfileView</button>
+
+
+
+
+
+
     
+
     `
 }
