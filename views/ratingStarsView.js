be783eb871282d860e836e0c913f1d0dad9533fa
Waqar1982 <waquar82@gmail.com>
2025-03-26 14:24:07 +0100

    a

--- views/ratingStarsView.js
+++ views/ratingStarsView.js
@@ -20,11 +20,19 @@ for (let i =0; i<maxStars; i++){
 if(i<ratingNumber){
     html+= /*HTML*/`
     <span class="star" data-value="1">&#9733</span>
+    <span class="star" data-value="2">&#9733</span>
+    <span class="star" data-value="3">&#9733</span>
+    <span class="star" data-value="4">&#9733</span>
+    <span class="star" data-value="5">&#9733</span>
     
     `
 } else {
     html+= /*HTML*/`
     <span class="star" data-value="1">&#9733</span>
+    <span class="star" data-value="2">&#9733</span>
+    <span class="star" data-value="3">&#9733</span>
+    <span class="star" data-value="4">&#9733</span>
+    <span class="star" data-value="5">&#9733</span>
     
     `
 }
