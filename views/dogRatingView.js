70f762b06271bdd9aa12bce11695f2adddc78ccf
Waqar1982 <waquar82@gmail.com>
2025-03-31 10:52:02 +0200

    Update dogRatingView.js

--- views/dogRatingView.js
+++ views/dogRatingView.js
@@ -12,3 +12,48 @@ function dogRatingView() {
     <h2>Hunderase<h2>
     `
 }
+
+function ratingStarsView() {
+    console.log("kjører ratingStarsView")
+app.innerHTML = /*HTML*/`
+
+
+<div class="rating">
+${printStars(5)}
+
+</div>
+
+
+`
+}
+
+function printStars (ratingNumber) {
+let maxStars = 5
+let html = ''
+
+for (let i =0; i<maxStars; i++){
+if(i<ratingNumber){
+    html+= /*HTML*/`
+    <span class="star" data-value="1">&#9733</span>
+    <span class="star" data-value="2">&#9733</span>
+    <span class="star" data-value="3">&#9733</span>
+    <span class="star" data-value="4">&#9733</span>
+    <span class="star" data-value="5">&#9733</span>
+    
+    `
+} else {
+    html+= /*HTML*/`
+    <span class="star" data-value="1">&#9733</span>
+    <span class="star" data-value="2">&#9733</span>
+    <span class="star" data-value="3">&#9733</span>
+    <span class="star" data-value="4">&#9733</span>
+    <span class="star" data-value="5">&#9733</span>
+    
+    `
+}
+return html
+}
+
+
+
+}
