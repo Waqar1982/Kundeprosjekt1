537752c066dd6542e0c8566fffcc5eb91ac78823
henninhm <henning.moller@vikenfiber.no>
2025-03-31 11:16:02 +0200

    Topliste sortering og modell-oppdatering

--- model.js
+++ model.js
@@ -76,23 +76,53 @@ data: {
     ],
 
 
-    dogs: [
+    dogs: [ //Kan starte med ett bilde
         { id: 1, rase: '', alder: '', størrelseVekt: '', favorittmat: '', rating: 4, ratings: 52, pictures: [] },
         { id: 2, rase: '', alder: '', størrelseVekt: '', favorittmat: '', rating: 3, ratings: 34, pictures: [] },
-        { id: 3, rase: '', alder: '', størrelseVekt: '', favorittmat: '', rating: 3.5, ratings: 41, pictures: [] },
+        { id: 3, rase: '', alder: '', størrelseVekt: '', favorittmat: '', rating: 3.5, ratings: 41, picture: 'dogpictures/dog3.avif' },
         { id: 4, rase: '', alder: '', størrelseVekt: '', favorittmat: '', rating: 5, ratings: 28, pictures: [] },
-        { id: 5, rase: '', alder: '', størrelseVekt: '', favorittmat: '', rating: 4.5, ratings: 33, pictures: [] },
+        { id: 5, rase: '', alder: '', størrelseVekt: '', favorittmat: '', rating: 4.5, ratings: 33, picture: 'dogpictures/dog5.avif' },
+        { id: 6, rase: '', alder: '', størrelseVekt: '', favorittmat: '', rating: 4, ratings: 52, pictures: [] },
+        { id: 7, rase: '', alder: '', størrelseVekt: '', favorittmat: '', rating: 3, ratings: 34, pictures: [] },
+        { id: 8, rase: '', alder: '', størrelseVekt: '', favorittmat: '', rating: 3.5, ratings: 41, pictures: [] },
+        { id: 9, rase: '', alder: '', størrelseVekt: '', favorittmat: '', rating: 5, ratings: 28, pictures: [] },
+        { id: 10, rase: '', alder: '', størrelseVekt: '', favorittmat: '', rating: 4.5, ratings: 33, picture: 'dogpictures/dog10.avif' },
+        { id: 11, rase: '', alder: '', størrelseVekt: '', favorittmat: '', rating: 4, ratings: 52, picture: 'dogpictures/dog11.avif' },
+        { id: 12, rase: '', alder: '', størrelseVekt: '', favorittmat: '', rating: 3, ratings: 34, picture: 'dogpictures/dog12.avif' },
+        { id: 13, rase: '', alder: '', størrelseVekt: '', favorittmat: '', rating: 3.5, ratings: 41, pictures: [] },
+        { id: 14, rase: '', alder: '', størrelseVekt: '', favorittmat: '', rating: 5, ratings: 28, pictures: [] },
+        { id: 15, rase: '', alder: '', størrelseVekt: '', favorittmat: '', rating: 4.5, ratings: 33, pictures: [] },
+        { id: 16, rase: '', alder: '', størrelseVekt: '', favorittmat: '', rating: 4.5, ratings: 33, pictures: [] },
     ],
 
-    topLists: [{
+    topLists: {
+        all: [1,2,3,4,5,6,7,8,9,10],
+        small: [3, 5, 10, 11, 12],
+        big: [],
+        shiba: [],
+        labrador: [],
+    },
+    /* topLists: [{
         listID: 1,
         name: 'all',
         topList: [],
     },{
         listID: 2,
         name: 'small',
+        topList: [3, 5, 10, 11, 12],
+    },{
+        listID: 3,
+        name: 'big',
         topList: [],
-    },],
+    },{
+        listID: 4,
+        name: 'shiba',
+        topList: [],
+    },{
+        listID: 5,
+        name: 'labrador',
+        topList: [],
+    },], */
 
     nyheter: [],
 
--- views/topListsView.js
+++ views/topListsView.js
@@ -1,33 +1,49 @@
 function topListsView() {
     app.innerHTML = /*html*/ `
+    <button onclick= "mainView()">Tilbake til Main View</button>
+
     <h1>Topp 10 Hunder</h1>
     <div id='all' class="topList"></div>
     
-    <h1>Topp 10 Små Hunder</h1>
+    <h1>Topp 5 Små Hunder</h1>
     <div id='small' class="topList"></div>
     
-    <h1>Topp 10 Store Hunder</h1>
+    <h1>Topp 5 Store Hunder</h1>
     <div id='big' class="topList"></div>
     
-    <h1>Topp 10 Adopterte Hunder</h1>
+    <h1>Topp 5 Adopterte Hunder</h1>
     <div id='adopted' class="topList"></div>
     
-    <h1>Topp 10 Shiba Inu</h1>
+    <h1>Topp 5 Shiba Inu</h1>
     <div id='shiba' class="topList"></div>
     
     `
     fillTopList('all')
+    fillTopList('small')
 }
 
 function fillTopList(type) {
+    if(type =='all') {  //for testing
+        list = document.getElementById(type)
+        for(i=1; i<11; i++) {
+            list.innerHTML += `
+            <div class="dogInList">
+                <img src="dogpictures/dog${i}.avif">
+            </div>
+            ` //Testbilder.
+            // Bilder skal komme fra topplister i modellen som kan endres med kode
+            // Bilder og/eller navn lenker til hundenes profil.
+        }
+        return
+    }
     list = document.getElementById(type)
-    for(i=1; i<11; i++) {
+    for(i=1; i<6; i++) {
+        dogID = model.data.topLists[type][i-1]
+        pic = model.data.dogs[dogID-1].picture
         list.innerHTML += `
         <div class="dogInList">
-            <img src="dogpictures/dog${i}.avif">
+            <img src=${pic}>
         </div>
-        ` //Testbilder.
-        // Bilder skal komme fra topplister i modellen som kan endres med kode
-        // Bilder og/eller navn lenker til hundenes profil.
+        `
     }
 }
