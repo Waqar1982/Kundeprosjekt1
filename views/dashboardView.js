b6e686915e4a00ffb56ad13001fca5a4b186fcf6
Waqar1982 <waquar82@gmail.com>
2025-04-02 13:01:30 +0200

    a

--- views/dashboardView.js
+++ views/dashboardView.js
@@ -3,27 +3,27 @@ function dashboardViewFunction() {
 
     <img src="dogpictures/dog1.avif" alt="Hundebilde" style="position: absolute; top: 120px; left: 10px; height: 150px;">
     <div class="stars" style="position: absolute; top: 305px; left: 60px; color: gold;">
-        ${'★'.repeat(5)}${'☆'.repeat(5 - 5)}
+    ${printStars(model.data.dogs[0].id)}
     </div>
 
     <img src="dogpictures/dog2.avif" alt="Hundebilde" style="position: absolute; top: 300px; left: 10px; height: 150px;">
     <div class="stars" style="position: absolute; top: 485px; left: 60px; color: gold;">
-        ${'★'.repeat(4)}${'☆'.repeat(5 - 4)}
+    ${printStars(model.data.dogs[0].id)}
     </div>
 
     <img src="dogpictures/dog3.avif" alt="Hundebilde" style="position: absolute; top: 480px; left: 10px; height: 150px;">
     <div class="stars" style="position: absolute; top: 665px; left: 60px; color: gold;">
-        ${'★'.repeat(3)}${'☆'.repeat(5 - 3)}
+    ${printStars(model.data.dogs[0].id)}
     </div>
 
     <img src="dogpictures/dog4.avif" alt="Hundebilde" style="position: absolute; top: 660px; left: 10px; height: 150px;">
     <div class="stars" style="position: absolute; top: 845px; left: 60px; color: gold;">
-        ${'★'.repeat(2)}${'☆'.repeat(5 - 2)}
+    ${printStars(model.data.dogs[0].id)}
     </div>
 
     <img src="dogpictures/dog5.avif" alt="Hundebilde" style="position: absolute; top: 840px; left: 10px; height: 150px;">
     <div class="stars" style="position: absolute; top: 1025px; left: 60px; color: gold;">
-        ${'★'.repeat(1)}${'☆'.repeat(5 - 1)}
+    ${printStars(model.data.dogs[0].id)}
     </div>
 
     <div id="topLeft">
@@ -40,6 +40,7 @@ function dashboardViewFunction() {
     <div id="todaysDog">
     <h2 style="margin: 0; text-align: center;">Dagens Hund:</h2>
     <img src="dogpictures/dog1.avif" alt="Dagens Hund">
+    ${printStars(model.data.dogs[0].id)}
     </div>
 
     <input type="text" id="newsBox" placeholder="Nyheter!">
