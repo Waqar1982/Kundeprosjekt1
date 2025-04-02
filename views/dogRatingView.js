88f46386239ddb477ad42e7db81003b1437a84e4
Waqar1982 <waquar82@gmail.com>
2025-04-02 10:04:27 +0200

    a

--- views/dogRatingView.js
+++ views/dogRatingView.js
@@ -6,16 +6,15 @@ function dogRatingView() {
 
     <button onclick="topListsView()">Flere Topplister</button>
     <input type="text" id="search" placeholder="Søk">
-    <button onclick="ratingSite()">Ratingside</button>
     <button onclick="myProfile()">Min Profil</button>
     <button onclick="gåTilNeste()">Gå til neste</button>
     <button onclick="tilbake()">Tilbake</button>
-    <h1>Ratingside<h1>
-    ${printStars(model.data.dogs[0].id)}
+     ${printStars(model.data.dogs[0].id)}
+     
+     <h2>Hundenavn<h2>
+     <h3>Gå til neste<h3>
+     <h2>Hunderase<h2>
+     `
     
-    <h2>Hundenavn<h2>
-    <h3>Gå til neste<h3>
-    <h2>Hunderase<h2>
-    `
 }
 
