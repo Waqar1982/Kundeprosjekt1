04c6ab344c2f14cdcfbd765cba8b2f0aa6d64a1b
Waqar1982 <waquar82@gmail.com>
2025-04-02 10:54:19 +0200

    a

--- views/dogRatingView.js
+++ views/dogRatingView.js
@@ -1,5 +1,5 @@
 function dogRatingView() {
-    dogID = 6
+    dogID = 4
     dog = model.data.dogs[dogID-1]
     app.innerHTML = /*html*/ `
    
@@ -9,15 +9,14 @@ function dogRatingView() {
     <button onclick="topListsView()">Flere Topplister</button>
     <input type="text" id="search" placeholder="Søk">
     <button onclick="myProfile()">Min Profil</button>
-    <button onclick="gåTilNeste()">Gå til neste</button>
-    <button onclick="tilbake()">Tilbake</button>
-
+   
     <h1>Ratingside<h1>
     <h2>${dog.name}<h2>
+    <button onclick="tilbake()">Tilbake</button>
     <img src=${dog.picture}>
+    <button onclick="gåTilNeste()", position center>Gå til neste</button>
     ${printStars(model.data.dogs[0].id)}
     
-    <h3>Gå til neste<h3>
     <h2>Rase: ${dog.race}<h2>
     `
 }
