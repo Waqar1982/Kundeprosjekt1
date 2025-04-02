7a1b5c198a8437686041c207e8668330adcc6921
Waqar1982 <waquar82@gmail.com>
2025-04-02 11:09:08 +0200

    a

--- views/dogRatingView.js
+++ views/dogRatingView.js
@@ -2,22 +2,25 @@ function dogRatingView() {
     dogID = 4
     dog = model.data.dogs[dogID-1]
     app.innerHTML = /*html*/ `
-   
-    
+   <style>
+    backgroudcolor: green
+
+   </style>
     <button onclick= "mainView()">Tilbake til Main View</button> <br> <br>
 
-    <button onclick="topListsView()">Flere Topplister</button>
+    <button onclick="topListsView()"style="cursor: pointer;">Flere Topplister</button>
     <input type="text" id="search" placeholder="Søk">
-    <button onclick="myProfile()">Min Profil</button>
+    <button onclick="myProfile()"style="cursor: pointer;">Min Profil</button>
    
-    <h1>Ratingside<h1>
+   
     <h2>${dog.name}<h2>
-    <button onclick="tilbake()">Tilbake</button>
+    <button onclick="tilbake()"style="cursor: pointer;">Tilbake</button>
     <img src=${dog.picture}>
-    <button onclick="gåTilNeste()", position center>Gå til neste</button>
+    <button onclick="gåTilNeste()"style="cursor: pointer;">Gå til neste</button>
     ${printStars(model.data.dogs[0].id)}
     
     <h2>Rase: ${dog.race}<h2>
+
     `
 }
 
