6bd386be49676187f7430818c82dc5c4ec3fa57c
henninhm <henning.moller@vikenfiber.no>
2025-04-07 11:18:16 +0200

    Loop for dashboard view

--- views/dashboardView.js
+++ views/dashboardView.js
@@ -1,31 +1,6 @@
 function dashboardViewFunction() {
     app.innerHTML = /*HTML*/ `
 
-    <img src="dogpictures/dog1.avif" alt="Hundebilde" style="position: absolute; top: 120px; left: 10px; height: 150px;">
-    <div class="stars" style="position: absolute; top: 305px; left: 60px; color: gold;">
-    ${printStars(model.data.dogs[0].id)}
-    </div>
-
-    <img src="dogpictures/dog2.avif" alt="Hundebilde" style="position: absolute; top: 300px; left: 10px; height: 150px;">
-    <div class="stars" style="position: absolute; top: 485px; left: 60px; color: gold;">
-    ${printStars(model.data.dogs[0].id)}
-    </div>
-
-    <img src="dogpictures/dog3.avif" alt="Hundebilde" style="position: absolute; top: 480px; left: 10px; height: 150px;">
-    <div class="stars" style="position: absolute; top: 665px; left: 60px; color: gold;">
-    ${printStars(model.data.dogs[0].id)}
-    </div>
-
-    <img src="dogpictures/dog4.avif" alt="Hundebilde" style="position: absolute; top: 660px; left: 10px; height: 150px;">
-    <div class="stars" style="position: absolute; top: 845px; left: 60px; color: gold;">
-    ${printStars(model.data.dogs[0].id)}
-    </div>
-
-    <img src="dogpictures/dog5.avif" alt="Hundebilde" style="position: absolute; top: 840px; left: 10px; height: 150px;">
-    <div class="stars" style="position: absolute; top: 1025px; left: 60px; color: gold;">
-    ${printStars(model.data.dogs[0].id)}
-    </div>
-
     <div id="topLeft">
     <button onclick="topLists()">Flere Topplister</button>
     <input type="text" id="search" placeholder="Søk">
@@ -33,7 +8,6 @@ function dashboardViewFunction() {
     <button onclick= "mainView()">Tilbake til Main View</button>
     <button id="profileButton" onclick="profileButton()">Profil</button>
     </div>
-
     
 
     <h1>Dashboard</h1>
@@ -48,4 +22,17 @@ function dashboardViewFunction() {
     <h2>Topp 5 hunder</h2>
     </div>
     `
+
+    dogs = model.data.dogs
+    for (i=0; i<5; i++) {
+        dog = dogs[i]
+        top1 = 120 + 180*i
+        top2 = 305 + 180*i
+        app.innerHTML += `
+        <img src="${dog.picture}" onclick="dogPageView(${i+1})" alt="Hundebilde" style="position: absolute; top: ${top1}px; left: 10px; height: 150px;">
+        <div class="stars" style="position: absolute; top: ${top2}px; left: 60px; color: gold;">
+        ${printStars(dog.id)}
+        </div>
+        `
+    }
 }
