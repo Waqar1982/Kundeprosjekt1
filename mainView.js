312af43093a74b5ee34f380b553a10f5cd95aa51
henninhm <henning.moller@vikenfiber.no>
2025-04-09 10:18:33 +0200

    Konsistente Indents

--- mainView.js
+++ mainView.js
@@ -1,56 +1,54 @@
 updateview()
 function header(){
   let profileButtonHTML = model.app.currentUserId == null ? "" : profileButton()
-return/*HTML*/`
+  return/*HTML*/`
    
-    <button onclick= "changePage('dashboard')">DashboardView</button> 
-    <button onclick= "changePage('dogPage')">dogPageView</button>
-    <button onclick= "changePage('ratingPage')">DogRatingView</button>
-    <button onclick= "changePage('ownerPage')">OwnerPagerView</button>
-    <button onclick= "changePage('topLists')">TopListsView</button>
-    <button onclick= "createAccountView()">CreateAccountView</button>
-    ${profileButtonHTML}
-    ${searchView()}
+  <button onclick= "changePage('dashboard')">DashboardView</button> 
+  <button onclick= "changePage('dogPage')">dogPageView</button>
+  <button onclick= "changePage('ratingPage')">DogRatingView</button>
+  <button onclick= "changePage('ownerPage')">OwnerPagerView</button>
+  <button onclick= "changePage('topLists')">TopListsView</button>
+  <button onclick= "createAccountView()">CreateAccountView</button>
+  ${profileButtonHTML}
+  ${searchView()}
   `
   
 }
 function updateview(){
-const content=chosenview()
+  const content=chosenview()
 
-app.innerHTML=/*HTML*/`
-<header>${header()}</header>
-<main>${content}</main>
-`
+  app.innerHTML=/*HTML*/`
+  <header>${header()}</header>
+  <main>${content}</main>
+  `
 }
 
 function chosenview(){
   console.log (model.app.currentPage)
-if(model.app.currentPage==model.app.page[0]){
-return startPageView()
-
-}else if(model.app.currentPage==model.app.page[1]){
-  return dashboardViewFunction()
-}else if(model.app.currentPage==model.app.page[2]){
-  return dogRating()
-}else if (model.app.currentPage==model.app.page[3]){
-  return dashboardViewFunction()
-}else if (model.app.currentPage==model.app.page[5]){
-  return dogPageView()
-}else if (model.app.currentPage==model.app.page[4]){
-  return ownerPageView()
-}else if (model.app.currentPage==model.app.page[6]){
-  return topListsView()
-}
-
+  if(model.app.currentPage==model.app.page[0]){
+    return startPageView()
+  }else if(model.app.currentPage==model.app.page[1]){
+    return dashboardViewFunction()
+  }else if(model.app.currentPage==model.app.page[2]){
+    return dogRating()
+  }else if (model.app.currentPage==model.app.page[3]){
+    return dashboardViewFunction()
+  }else if (model.app.currentPage==model.app.page[5]){
+    return dogPageView()
+  }else if (model.app.currentPage==model.app.page[4]){
+    return ownerPageView()
+  }else if (model.app.currentPage==model.app.page[6]){
+    return topListsView()
+  }
 }
 function changePage(newPage){
-model.app.currentPage=newPage;
-updateview()
+  model.app.currentPage=newPage;
+  updateview()
 }
 
 function changeCurrentDog(newDogID){
-   model.inputs.currentDogID=newDogID;
-   changePage('dogPage')
+  model.inputs.currentDogID=newDogID;
+  changePage('dogPage')
 }
 
 function changeRatingDog(newDogID){
