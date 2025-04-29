f2e8c541c6531f482f63ac12039da4a019205da5
Waqar1982 <waquar82@gmail.com>
2025-04-29 10:19:18 +0200

    a

--- views/createAccountView.js
+++ views/createAccountView.js
@@ -9,13 +9,13 @@ function createAccountView() {
     <hr>
 </div>
 <div class="form-container">
-    <input type="text" id="firstName" placeholder="Fornavn" style= "width: 50%;" onchange="model.inputs.createUser.firstName=this.value">
-    <input type="text" id="lastName" placeholder="Etternavn" style= "width: 50%;" onchange="model.inputs.createUser.lastName=this.value">
-    <input type="text" id="email" placeholder="E-Post" style= "width: 50%;" onchange="model.inputs.createUser.email=this.value">
-    <input type="text" id="password" placeholder="Passord" style= "width: 50%;" onchange="model.inputs.createUser.password=this.value">
-    <input type="text" id="phone" placeholder="Telefonnummer" style= "width: 50%;" onchange="model.inputs.createUser.number=this.value">
+    <input type="text" id="firstName" placeholder="Fornavn" style= "width: 20%;" onchange="model.inputs.createUser.firstName=this.value">
+    <input type="text" id="lastName" placeholder="Etternavn" style= "width: 20%;" onchange="model.inputs.createUser.lastName=this.value">
+    <input type="text" id="email" placeholder="E-Post" style= "width: 20%;" onchange="model.inputs.createUser.email=this.value">
+    <input type="text" id="password" placeholder="Passord" style= "width: 20%;" onchange="model.inputs.createUser.password=this.value">
+    <input type="text" id="phone" placeholder="Telefonnummer" style= "width: 20%;" onchange="model.inputs.createUser.number=this.value">
    
-    <button onclick="handleUserRegistration()" style= "width: 50%;">Registrer</button>
+    <button onclick="handleUserRegistration()" style= "width: 20%;">Registrer</button>
     </div> 
     `;
     return html
