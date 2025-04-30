26e568b30d96c6cb19c165a2c07c05c5b39167a4
CsLy0 <marius.sundt.skullestad@gmail.com>
2025-04-30 10:04:29 +0200

    32

--- views/dogRatingView.js
+++ views/dogRatingView.js
@@ -2,10 +2,7 @@ function dogRating() {
     
     let html=""
     dogID = model.inputs.ratingDogID;
-    /* if(idInput && idInput < model.data.dogs.length+1) {
-        dogID = idInput
-    }*/
-    //let dog = model.data.dogs[dogID-1];
+   
     const dog=model.data.dogs.find(element=>element.id==dogID)
     let stars=""
     if(model.app.currentUserId==null){
@@ -15,7 +12,7 @@ function dogRating() {
 
     }
     html += /*html*/ `
-    
+    <h1> Rate min hund</h1>
     <h2>${dog.name}<h2>
     <button onclick="changeRatingDog(dogID, 'down')"style="cursor: pointer;">Tilbake</button>
     <img src=${dog.picture}>
@@ -25,16 +22,9 @@ function dogRating() {
     <h2>Rase: ${dog.race}<h2>
     `;
     return html
-
-    /*const darkModeToggle = document.getElementById('darkModeToggle');
-            darkModeToggle.addEventListener('click', function () {
-                document.body.classList.toggle('dark-mode');
-        // Re-render the stars after toggling dark mode
-        document.getElementById('stars').innerHTML = printStars(model.data.dogs[0].id);
-    });*/
 }
 
-// Update the stars in dogRatingView after clicking on stars in ratingStarsView
+
 
 function updateDogRatingViewStars() {
     document.getElementById('stars').innerHTML = printStars(model.inputs.currentDogID);
